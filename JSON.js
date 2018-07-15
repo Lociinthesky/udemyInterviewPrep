449cede0f3b2de2514c30a6f1a569339f91ee2cb
Lance Tschirhart <lociinthesky@gmail.com>
2018-07-15 17:15:17 -0500

    few more problems

---
+++ JSON.js
@@ -0,0 +1,22 @@
+//WHAT IS JSON
+//WHICH JSON TYPES ARENT PERMITTED?
+
+const myJsonObj = {
+	"myString": "hello world"
+	"myNumber": 1234.5667,
+	"myNull": null,
+	"myBoolean": true,
+	"myArray": [20, 30, "orange"],
+	"myObject": {
+		"name": "Sam",
+		"age": 30
+	}
+	//myFunction: [myFunction].  <--- Only functions and undefined are not allowed.
+	//undefined: [undefined]
+}
+
+// JSON light weight format for transferring data
+// 1-easy for humans to understand
+// 2-easy for comps to parse and generate
+
+	
--- singleDoubleInvoke.js
+++ singleDoubleInvoke.js
@@ -1,3 +1,7 @@
+//getTotal(10, 20)
+//getTotal(10)(20)
+//Should both work
+
 function getTotal(...args){
   var num1 = args[0];
   if ( args.length === 1 ) {
@@ -8,6 +12,21 @@ function getTotal(...args){
   return num1 + args[1]
 }
 
+//ALTERNATIVELY:
+
+
+function getTotal(){
+  var args = Array.prototype.slice.call(arguments);
+
+  if ( args.length === 2 ) {
+  	return args[0] + args[1];
+  }
+  else if ( args.length === 1 ) {
+  	return function(num2) {
+  		return args[0] + num2;
+  	}
+  }
+}
 
 //getTotal(10, 20)
 //getTotal(10)(20)
